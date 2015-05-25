a583b6ab9116f79a1150d71c9ed54afe58e16072
xiaeryu <xiaeryu@nus.edu.sg>
2015-05-25 13:14:36 +0800

    Create wordFrequency.sh

---
+++ wordFrequency.sh
@@ -0,0 +1,7 @@
+################################################################################
+## LeetCode Number 192. Word Frequency
+## Write a bash script to calculate the frequency of each word in a text file words.txt
+## The problem is elaborated here: https://leetcode.com/problems/word-frequency/
+################################################################################
+
+sed 's/[[:space:]]\+/\n/g' words.txt | sort | uniq -c | sort -nrk1 | awk '{print $2 "\t" $1}'
