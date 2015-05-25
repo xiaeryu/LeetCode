dbf3bd60d8bd29fec7728363d611fe4a511af936
xiaeryu <xiaeryu@nus.edu.sg>
2015-05-25 13:40:05 +0800

    Update transposeFile.sh

--- transposeFile.sh
+++ transposeFile.sh
@@ -1,3 +1,7 @@
+#########################################################################
+## LeetCode Number 194 Transpose File
+## Problem description can be found here:
+## https://leetcode.com/problems/transpose-file/
+#########################################################################
 
-
-awk '{printf $1 " "}' test | sed 's/ $/\n/'; awk '{printf $2 " "}' test | sed 's/ $/\n/'
+awk '{printf $1 " "}' file.txt | sed 's/ $/\n/'; awk '{printf $2 " "}' file.txt | sed 's/ $/\n/'
