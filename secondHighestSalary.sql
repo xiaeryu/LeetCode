1f38a8a720eb985dc6ec49e8bccb7d720cfc36b3
xiaeryu <xiaeryu@nus.edu.sg>
2015-05-25 15:12:58 +0800

    Update secondHighestSalary.sql

--- secondHighestSalary.sql
+++ secondHighestSalary.sql
@@ -1,3 +1,10 @@
+#########################################################################
+## LeetCode Number 176 Second Highest Salary
+## Problem description can be found here:
+## https://leetcode.com/problems/second-highest-salary/
+## MySQL query statement
+#########################################################################
+
 Select MAX(Salary)
 FROM Employee
 WHERE Salary < (Select MAX(Salary) FROM Employee)
