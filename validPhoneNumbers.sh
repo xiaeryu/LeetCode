12c7ea56b913d31752e2ca590883eb140f8dbfc5
xiaeryu <xiaeryu@nus.edu.sg>
2015-05-25 13:28:59 +0800

    Create validPhoneNumbers.sh

---
+++ validPhoneNumbers.sh
@@ -0,0 +1,7 @@
+######################################################################
+## LeetCode Number 193 Valid Phone Numbers 
+## Problem description can be found here:
+## https://leetcode.com/problems/valid-phone-numbers/
+######################################################################
+
+grep -v '^ \+' file.txt | grep -v ' \+$' | grep '[0-9]\{3\}-[0-9]\{3\}-[0-9]\{4\}\|([0-9]\{3\})\ [0-9]\{3\}-[0-9]\{4\}'
