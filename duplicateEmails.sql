54f5c4e4982e2481b24328bef35db77a3c65b5c7
xiaeryu <xiaeryu@nus.edu.sg>
2015-05-26 19:05:32 +0800

    Update duplicateEmails.sql

--- duplicateEmails.sql
+++ duplicateEmails.sql
@@ -1,3 +1,10 @@
+#########################################################################
+## LeetCode Number 182 Duplicate Emails
+## Problem description can be found here:
+## https://leetcode.com/problems/duplicate-emails/
+## MySQL query statement
+#########################################################################
+
 # Write your MySQL query statement below
 SELECT Email
 FROM(
