db9e293e4df3f0920601b15127501c9bc502a126
xiaeryu <xiaeryu@nus.edu.sg>
2015-05-30 16:02:18 +0800

    Create factorialTrailingZeroes.cpp

---
+++ factorialTrailingZeroes.cpp
@@ -0,0 +1,17 @@
+#########################################################################
+## LeetCode Number 172 Factorial Trailing Zeroes 
+## Problem description can be found here:
+## https://leetcode.com/problems/factorial-trailing-zeroes/
+## C++ script
+#########################################################################
+
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        int sum = 0;
+        for(int i=1;pow(5,i)<=n;i++){
+            sum += (n/pow(5,i));
+        }
+        return sum;
+    }
+};
