95e92d4031969beb14208c089ed62de69d6e47e4
xiaeryu <xiaeryu@nus.edu.sg>
2015-06-06 11:07:49 +0800

    Update subsetsII.py

--- subsetsII.py
+++ subsetsII.py
@@ -4,3 +4,25 @@
 ## https://leetcode.com/problems/subsets-ii/
 ## Python script
 #########################################################################
+
+class Solution:
+    # @param {integer[]} nums
+    # @return {integer[][]}
+    def subsetsWithDup(self, nums):
+        if len(nums) == 0:
+            return [[]]
+        
+        nums.sort()
+        out = [[],[nums[0]]]
+
+        for i in range(1,len(nums)):
+            tmp = []
+            for arr in out:
+                here = arr[:]
+                here.append(nums[i])
+                tmp.append(here[:])
+            for item in tmp:
+                if item not in out:
+                    out.append(item[:])
+        
+        return out
