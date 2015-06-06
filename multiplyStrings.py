0dbe32a1d490ee30f3ec9d85f15c8c5c4317aa13
xiaeryu <xiaeryu@nus.edu.sg>
2015-06-06 10:59:23 +0800

    Create multiplyStrings.py

---
+++ multiplyStrings.py
@@ -0,0 +1,13 @@
+#########################################################################
+## LeetCode Number 43 Multiply Strings 
+## Problem description can be found here:
+## https://leetcode.com/problems/multiply-strings/
+## Python script
+#########################################################################
+
+class Solution:
+    # @param {string} num1
+    # @param {string} num2
+    # @return {string}
+    def multiply(self, num1, num2):
+        return str(int(num1)*int(num2))
