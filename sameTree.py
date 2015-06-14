638bad8875ba145acdbcb6a1716f63c160d75f66
xiaeryu <xiaeryu@nus.edu.sg>
2015-06-14 19:34:38 +0800

    Create sameTree.py

---
+++ sameTree.py
@@ -0,0 +1,27 @@
+#########################################################################
+## LeetCode Number 100 Same Tree
+## Problem description can be found here:
+## https://leetcode.com/problems/same-tree/
+## Python script
+#########################################################################
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    # @param {TreeNode} p
+    # @param {TreeNode} q
+    # @return {boolean}
+    def isSameTree(self, p, q):
+        if (not p) and (not q):
+            return True
+        elif (not p) or (not q):
+            return False
+        
+        if p.val != q.val:
+            return False
+        return self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
