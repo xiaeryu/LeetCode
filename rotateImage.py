237cd173380afa6ad1681a5a3c668eba88065ca1
xiaeryu <xiaeryu@nus.edu.sg>
2015-06-18 22:32:02 +0800

    Update rotateImage.py

--- rotateImage.py
+++ rotateImage.py
@@ -1,6 +1,17 @@
 #########################################################################
 ## LeetCode Number 48 Rotate Image 
 ## Problem description can be found here:
-## https://leetcode.com/problems/add-binary/
+## https://leetcode.com/problems/rotate-image/
 ## Python script
 #########################################################################
+
+class Solution:
+    # @param {integer[][]} matrix
+    # @return {void} Do not return anything, modify matrix in-place instead.
+    def rotate(self, matrix):
+        matrix.reverse()
+        nrow = len(matrix)
+        ncol = len(matrix[0])
+        for i in range(nrow):
+            for j in range(i):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
